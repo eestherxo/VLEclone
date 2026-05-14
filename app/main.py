13bc2d6e0ccbb29ffaf583901d762c83f9806ecf
Estherlee Redman <estherlee.redman.0@gmail.com>
2026-05-14 01:18:41 -0500

    added blueprints for calendar events & asssignments to main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from .db import config
-from .routes import auth_bp, course_bp
+from .routes import auth_bp, course_bp, event_bp
 
 
 
@@ -18,3 +18,4 @@ jwt = JWTManager(app)
 # Endpoints
 app.register_blueprint(auth_bp)
 app.register_blueprint(course_bp)
+app.register_blueprint(event_bp)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,5 @@
 from .auth import auth_bp
 from .courses import course_bp
+from .events import event_bp
 
-__all__ = ['auth_bp', 'course_bp']
+__all__ = ['auth_bp', 'course_bp', 'event_bp']
