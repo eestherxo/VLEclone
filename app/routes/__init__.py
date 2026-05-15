d7207fdb3cf8e239dcdc90c5780d5767be7bd4e8
Jason Matthews <jasonmatthews@mac.mynetworksettings.com>
2026-05-15 18:47:45 -0400

    changes to init

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from .auth import auth_bp
 from .courses import course_bp
 from .events import event_bp
+from .forum import forum_bp
+from .threads import thread_bp
 
-__all__ = ['auth_bp', 'course_bp', 'event_bp']
+__all__ = ['auth_bp', 'course_bp', 'event_bp', 'forum_bp', 'thread_bp']
