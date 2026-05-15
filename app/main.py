6face2c2523553b2cd0f6771c1f2ef178c5ba0c8
Jason Matthews <jasonmatthews@mac.mynetworksettings.com>
2026-05-15 18:42:14 -0400

    added endpoints

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from .db import config
-from .routes import auth_bp, course_bp, event_bp
+from .routes import auth_bp, course_bp, event_bp, forum_bp, thread_bp
 
 
 
@@ -19,3 +19,5 @@ jwt = JWTManager(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(course_bp)
 app.register_blueprint(event_bp)
+app.register_blueprint(forum_bp)
+app.register_blueprint(thread_bp)
