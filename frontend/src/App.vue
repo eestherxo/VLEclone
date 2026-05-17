130b93fbaa0c4cd6cb743bc0f28b3e3f779ae7b6
Lamar4546 <lamarshaw18@gmail.com>
2026-05-17 13:39:42 -0500

    NavBar edit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,10 +15,17 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 import SideBar from './components/SideBar.vue'
 
-const isAuthenticated = computed(() => !!localStorage.getItem('token'))
+const router = useRouter()
+const isAuthenticated = ref(!!localStorage.getItem('token'))
 const sidebarCollapsed = ref(false)
+
+// re-check auth on every route change
+router.afterEach(() => {
+  isAuthenticated.value = !!localStorage.getItem('token')
+})
 </script>
